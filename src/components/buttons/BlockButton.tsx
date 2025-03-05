a1dbfbe545b6b5f90c997acd051a2a2e09247d3b
Jaete <jaetev@outlook.com>
2025-03-05 16:13:22 -0300

    fix:  alter lint rules; fix property name

--- src/components/buttons/BlockButton.tsx
+++ src/components/buttons/BlockButton.tsx
@@ -2,6 +2,7 @@ import styles from './BlockButton.module.scss'
 
 interface BlockButtonProps {
     text: string,
+    size?: string
 }
 
 export default function BlockButton({text}: BlockButtonProps){
